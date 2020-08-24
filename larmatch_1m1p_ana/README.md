a73c3e48f2b8195ce8ec4222587ecea9208a898f
twongjirad <taritree.wongjirad@gmail.com>
2020-08-23 23:09:38 -0400

    added run scripts

---
+++ larmatch_1m1p_ana/README.md
@@ -0,0 +1,12 @@
+# LARMATCH 1M1P ANA
+
+Scripts to run through 1m1p-filtered files.
+
+We get a sample of data files in order to:
+
+1) make plot demonstrating de/dx distribution for two prongs attached to DL vertex
+2) make plot demonstrating PID separation in data
+
+To compare, we also want to run on MC examples -- dlfiltered MC overlay files.
+
+1) equivalent plots
---
+++ larmatch_1m1p_ana/submit_batch_kps_larmatch.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# slurm submission script for making larmatch training data
+
+#SBATCH --job-name=larmatch
+#SBATCH --output=kps_batch_mc_tmw1.log
+#SBATCH --mem-per-cpu=4000
+#SBATCH --time=1-00:00:00
+#SBATCH --array=110-250
+
+container=/cluster/tufts/wongjiradlab/twongj01/ubdl/larflow/larmatchnet/grid_deploy_scripts/singularity_pytorch1.3cpu.simg
+RUN_DLANA_DIR=/cluster/tufts/wongjiradlab/twongj01/ubdl-ana/larmatch_1m1p_ana/
+OFFSET=0
+STRIDE=10
+
+SAMPLE_NAME=mcc9_v29e_dl_run3_G1_bnb_dlfilter_1m1p # 2503
+
+module load singularity
+srun singularity exec ${container} bash -c "cd ${RUN_DLANA_DIR} && source run_batch_kps_larmatch.sh $OFFSET $STRIDE $SAMPLE_NAME ${RUN_DLANA_DIR}"
+
