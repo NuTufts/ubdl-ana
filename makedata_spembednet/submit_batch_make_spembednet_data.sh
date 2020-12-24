0aa5db520cb62b8b003eb83bf91b7d4e146193cb
twongjirad <taritree.wongjirad@gmail.com>
2020-12-24 07:40:38 -0500

    update for new, cleaned up training data

--- makedata_spembednet/submit_batch_make_spembednet_data.sh
+++ makedata_spembednet/submit_batch_make_spembednet_data.sh
@@ -2,20 +2,23 @@
 
 # slurm submission script for making larmatch training data
 
-#SBATCH --job-name=larmatch
+#SBATCH --job-name=sp3demb
 #SBATCH --output=kps_batch_bnbnu_rerun.log
-#SBATCH --mem-per-cpu=2000
-#SBATCH --cpus-per-task=4
+#SBATCH --mem-per-cpu=8000
+#SBATCH --cpus-per-task=1
 #SBATCH --time=1-00:00:00
-#SBATCH --array=0-46
+#SBATCH --array=0
 
 container=/cluster/tufts/wongjiradlab/twongj01/ubdl/larflow/larmatchnet/grid_deploy_scripts/singularity_pytorch1.3cpu.simg
 RUN_DLANA_DIR=/cluster/tufts/wongjiradlab/twongj01/ubdl-ana/makedata_spembednet/
 OFFSET=0
 STRIDE=10
 
+#SAMPLE_NAME=mcc9_v13_bnbnue_corsika_training
 SAMPLE_NAME=mcc9_v13_bnbnue_corsika_valid
 
+
 module load singularity
-srun singularity exec ${container} bash -c "cd ${RUN_DLANA_DIR} && source run_batch_kps_larmatch.sh $OFFSET $STRIDE $SAMPLE_NAME"
+#srun singularity exec ${container} bash -c "cd ${RUN_DLANA_DIR} && source run_batch_kps_larmatch.sh $OFFSET $STRIDE $SAMPLE_NAME"
+srun singularity exec ${container} bash -c "cd ${RUN_DLANA_DIR} && source run_batch_truehit_training_voxels.sh $OFFSET $STRIDE $SAMPLE_NAME"
 
