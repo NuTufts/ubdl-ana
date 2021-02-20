6ef16d2e16f440d63307b9648020cd8d3237bb92
twongjirad <taritree.wongjirad@gmail.com>
2021-02-20 07:25:05 -0500

    add enu true plot with modes

--- dev_1e1p_selection/plot_enu.py
+++ dev_1e1p_selection/plot_enu.py
@@ -11,10 +11,10 @@ fcolors = {"intrinsicnue":rt.kRed-3,
            "bnbnu":rt.kBlue-3}
 fpot = {"bnbnu":4.159516125e+19,
         "intrinsicnue":4.613240272e+22}
-fill_order = ["intrinsicnue","bnbnu"]        
+fill_order = ["intrinsicnue","bnbnu"]
+#fill_order = ["bnbnu"]
 
 sample_names = [ "is1eVA","1e1p","all" ]
-
 mode_names = ["ccqe",
               "ccres",
               "ccother",
@@ -22,7 +22,6 @@ mode_names = ["ccqe",
               "ncres",
               "ncother",
               "all"]
-
 mode_styles = {"ccqe":1001,
                "ccres":3002,
                "ccother":3144,
@@ -30,7 +29,6 @@ mode_styles = {"ccqe":1001,
                "ncres":3007,
                "ncother":3025,
                "all":3003}
-
 
 rfile = {}
 for nutype in ftypes:
@@ -42,23 +40,35 @@ hstack_v = {}
 tlen_v = []
 for s in sample_names:
     canvas[s] = rt.TCanvas("c%s"%(s),s,1200,600)
-    # load hists
-    hname = "hnshower_%s"%(s)
-    hstack = rt.THStack(hname+"_stack","")    
-    for nutype in fill_order:
-        h = rfile[nutype].Get( hname )
-        h.SetFillColor(fcolors[nutype])
-        h.SetFillStyle(3003)
-        scale = pot/fpot[nutype]
-        h.Scale(scale)
-        print nutype," ",s,": ",h.Integral()
-        hstack.Add(h)        
-        hists[(nutype,s)] = h
+
+    # building up this stack
+    hstack = rt.THStack("h"+s+"_stack","")
+    all_tot = 0. # a check
+    stack_tot = 0.
     
+    # load hists
+    for mode in mode_names:
+        #hname = "hEnu_%s_vertexactcut_%s"%(s,mode)
+        hname = "hEnu_%s_allrecocut_%s"%(s,mode)        
+
+        for nutype in fill_order:
+            h = rfile[nutype].Get( hname )
+            h.SetFillColor(fcolors[nutype])
+            h.SetFillStyle(mode_styles[mode])
+            scale = pot/fpot[nutype]
+            h.Scale(scale)
+            print nutype," ",s," ",mode,": ",h.Integral()
+            if mode != "all":
+                hstack.Add(h)
+                stack_tot += h.Integral()
+            else:
+                all_tot += h.Integral()
+                print "  all <=500 Mev: ",h.Integral(1,h.GetXaxis().FindBin(500))
+            hists[(nutype,s,mode)] = h
+            
     hstack.Draw("hist")
-    pred = h.Integral()
+    print "stack tot=", stack_tot," vs. all tot=",all_tot        
     canvas[s].Draw()
-    print hname,": ",pred
     hstack_v[s] = hstack
     
 print "enter to end"
